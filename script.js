39ea879cf47d872e5da8fb497734c83f0c9a2b70
Mohit Kumar <jhamohit111@gmail.com>
2022-01-20 09:27:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 window.onload = function () {
     var inputlist = document.getElementById("suduko");
-    var i = 0, j = 0,k=0,l=0,m=0,n=0,o=0;
-    setInterval(checkRangeOfInputs,200);
-    setInterval(checkDuplicatesInRow,300);
-    setInterval(checkDuplicatesInColumn,300);
+    var i = 0, j = 0, k = 0, l = 0, m = 0, n = 0, o = 0;
+    setInterval(checkRangeOfInputs, 200);
+    setInterval(checkDuplicatesInRow, 300);
+    setInterval(checkDuplicatesInColumn, 300);
+    setInterval(checkDuplicatesIn3by3,300);
 
 
 
-    function checkDuplicatesInRow()
-    {
+    function checkDuplicatesInRow() {
         for (l = 0; l < 9; l++) {
             var d = inputlist.getElementsByTagName("TR")[l];
             for (m = 0; m < 9; m++) {
                 var e = d.getElementsByTagName("TD")[m];
                 var f = e.getElementsByTagName("input")[0];
-                for(n=0;n<9;n++)
-                {
+                for (n = 0; n < 9; n++) {
                     var g = d.getElementsByTagName("TD")[n];
                     var h = g.getElementsByTagName("input")[0];
-                    if(((h.value == f.value)&&(m !=n ))&&(h.value != ""))
-                    {
-                        h.style.color="red";
-                        h.style.fontWeight ="bold";
+                    if (((h.value == f.value) && (m != n)) && (h.value != "")) {
+                        h.style.color = "red";
+                        h.style.fontWeight = "bold";
                     }
 
                 }
@@ -29,29 +27,244 @@ window.onload = function () {
         }
     }
 
-    function checkDuplicatesInColumn(){
-        for(o =0;o<9;o++)
-        {
-            for(p = 0;p<9;p++)
-            {
+    function checkDuplicatesInColumn() {
+        for (o = 0; o < 9; o++) {
+            for (p = 0; p < 9; p++) {
                 var r = inputlist.getElementsByTagName("TR")[p];
                 var s = r.getElementsByTagName("TD")[o];
                 var t = s.getElementsByTagName("input")[0];
-                for(q=0;q<9;q++)
-                {
+                for (q = 0; q < 9; q++) {
                     var u = inputlist.getElementsByTagName("TR")[q];
-                        var w = u.getElementsByTagName("TD")[o];
-                        var x = w.getElementsByTagName("input")[0];
-                        if(((x.value == t.value)&&(p !=q ))&&(x.value != ""))
-                    {
-                        x.style.color="red";
-                        x.style.fontWeight ="bold";
+                    var w = u.getElementsByTagName("TD")[o];
+                    var x = w.getElementsByTagName("input")[0];
+                    if (((x.value == t.value) && (p != q)) && (x.value != "")) {
+                        x.style.color = "red";
+                        x.style.fontWeight = "bold";
                     }
-                    
+
                 }
             }
         }
     }
+
+    function checkDuplicatesIn3by3() {
+        var row = 0, column = 0; rowRange = 2; columnRange = 2;
+
+        for (rowtemp = row; rowtemp <= rowRange; rowtemp++) {
+            for (coltemp = column; coltemp <= columnRange; coltemp++) {
+                var aa = inputlist.getElementsByTagName("TR")[rowtemp];
+                var ab = aa.getElementsByTagName("TD")[coltemp];
+                var bb = ab.getElementsByTagName("input")[0];
+                for (rowtemp1 = row; rowtemp1 <= rowRange; rowtemp1++) {
+                    for (coltemp1 = column; coltemp1 <= columnRange; coltemp1++) {
+                        var ac = inputlist.getElementsByTagName("TR")[rowtemp1];
+                        var ad = ac.getElementsByTagName("TD")[coltemp1];
+                        var cc = ad.getElementsByTagName("input")[0];
+                        if((bb.value == cc.value)&&(bb.value != ""))
+                        {
+                            if(!((rowtemp == rowtemp1)&&(coltemp ==  coltemp1)))
+                            {
+                                bb.style.color = "red";
+                                cc.style.color = "red";
+                            }
+                        }
+                    }
+                }
+                
+            }
+            for (coltemp = column+3; coltemp <= columnRange+3; coltemp++) {
+                var aa = inputlist.getElementsByTagName("TR")[rowtemp];
+                var ab = aa.getElementsByTagName("TD")[coltemp];
+                var bb = ab.getElementsByTagName("input")[0];
+                for (rowtemp1 = row; rowtemp1 <= rowRange; rowtemp1++) {
+                    for (coltemp1 = column+3; coltemp1 <= columnRange+3; coltemp1++) {
+                        var ac = inputlist.getElementsByTagName("TR")[rowtemp1];
+                        var ad = ac.getElementsByTagName("TD")[coltemp1];
+                        var cc = ad.getElementsByTagName("input")[0];
+                        if((bb.value == cc.value)&&(bb.value != ""))
+                        {
+                            if(!((rowtemp == rowtemp1)&&(coltemp ==  coltemp1)))
+                            {
+                                bb.style.color = "red";
+                                cc.style.color = "red";
+                            }
+                        }
+                    }
+                }
+                
+            }
+            for (coltemp = column+6; coltemp <= columnRange+6; coltemp++) {
+                var aa = inputlist.getElementsByTagName("TR")[rowtemp];
+                var ab = aa.getElementsByTagName("TD")[coltemp];
+                var bb = ab.getElementsByTagName("input")[0];
+                for (rowtemp1 = row; rowtemp1 <= rowRange; rowtemp1++) {
+                    for (coltemp1 = column+6; coltemp1 <= columnRange+6; coltemp1++) {
+                        var ac = inputlist.getElementsByTagName("TR")[rowtemp1];
+                        var ad = ac.getElementsByTagName("TD")[coltemp1];
+                        var cc = ad.getElementsByTagName("input")[0];
+                        if((bb.value == cc.value)&&(bb.value != ""))
+                        {
+                            if(!((rowtemp == rowtemp1)&&(coltemp ==  coltemp1)))
+                            {
+                                bb.style.color = "red";
+                                cc.style.color = "red";
+                            }
+                        }
+                    }
+                }
+                
+            } 
+        }
+
+
+
+
+
+
+        row  = row +3;  
+        rowRange = rowRange+3;
+
+        for (rowtemp = row; rowtemp <= rowRange; rowtemp++) {
+            for (coltemp = column; coltemp <= columnRange; coltemp++) {
+                var aa = inputlist.getElementsByTagName("TR")[rowtemp];
+                var ab = aa.getElementsByTagName("TD")[coltemp];
+                var bb = ab.getElementsByTagName("input")[0];
+                for (rowtemp1 = row; rowtemp1 <= rowRange; rowtemp1++) {
+                    for (coltemp1 = column; coltemp1 <= columnRange; coltemp1++) {
+                        var ac = inputlist.getElementsByTagName("TR")[rowtemp1];
+                        var ad = ac.getElementsByTagName("TD")[coltemp1];
+                        var cc = ad.getElementsByTagName("input")[0];
+                        if((bb.value == cc.value)&&(bb.value != ""))
+                        {
+                            if(!((rowtemp == rowtemp1)&&(coltemp ==  coltemp1)))
+                            {
+                                bb.style.color = "red";
+                                cc.style.color = "red";
+                            }
+                        }
+                    }
+                }
+                
+            }
+            for (coltemp = column+3; coltemp <= columnRange+3; coltemp++) {
+                var aa = inputlist.getElementsByTagName("TR")[rowtemp];
+                var ab = aa.getElementsByTagName("TD")[coltemp];
+                var bb = ab.getElementsByTagName("input")[0];
+                for (rowtemp1 = row; rowtemp1 <= rowRange; rowtemp1++) {
+                    for (coltemp1 = column+3; coltemp1 <= columnRange+3; coltemp1++) {
+                        var ac = inputlist.getElementsByTagName("TR")[rowtemp1];
+                        var ad = ac.getElementsByTagName("TD")[coltemp1];
+                        var cc = ad.getElementsByTagName("input")[0];
+                        if((bb.value == cc.value)&&(bb.value != ""))
+                        {
+                            if(!((rowtemp == rowtemp1)&&(coltemp ==  coltemp1)))
+                            {
+                                bb.style.color = "red";
+                                cc.style.color = "red";
+                            }
+                        }
+                    }
+                }
+                
+            }
+            for (coltemp = column+6; coltemp <= columnRange+6; coltemp++) {
+                var aa = inputlist.getElementsByTagName("TR")[rowtemp];
+                var ab = aa.getElementsByTagName("TD")[coltemp];
+                var bb = ab.getElementsByTagName("input")[0];
+                for (rowtemp1 = row; rowtemp1 <= rowRange; rowtemp1++) {
+                    for (coltemp1 = column+6; coltemp1 <= columnRange+6; coltemp1++) {
+                        var ac = inputlist.getElementsByTagName("TR")[rowtemp1];
+                        var ad = ac.getElementsByTagName("TD")[coltemp1];
+                        var cc = ad.getElementsByTagName("input")[0];
+                        if((bb.value == cc.value)&&(bb.value != ""))
+                        {
+                            if(!((rowtemp == rowtemp1)&&(coltemp ==  coltemp1)))
+                            {
+                                bb.style.color = "red";
+                                cc.style.color = "red";
+                            }
+                        }
+                    }
+                }
+                
+            } 
+        }
+
+
+
+        row = row+3; 
+        rowRange = rowRange +3;
+
+        for (rowtemp = row; rowtemp <= rowRange; rowtemp++) {
+            for (coltemp = column; coltemp <= columnRange; coltemp++) {
+                var aa = inputlist.getElementsByTagName("TR")[rowtemp];
+                var ab = aa.getElementsByTagName("TD")[coltemp];
+                var bb = ab.getElementsByTagName("input")[0];
+                for (rowtemp1 = row; rowtemp1 <= rowRange; rowtemp1++) {
+                    for (coltemp1 = column; coltemp1 <= columnRange; coltemp1++) {
+                        var ac = inputlist.getElementsByTagName("TR")[rowtemp1];
+                        var ad = ac.getElementsByTagName("TD")[coltemp1];
+                        var cc = ad.getElementsByTagName("input")[0];
+                        if((bb.value == cc.value)&&(bb.value != ""))
+                        {
+                            if(!((rowtemp == rowtemp1)&&(coltemp ==  coltemp1)))
+                            {
+                                bb.style.color = "red";
+                                cc.style.color = "red";
+                            }
+                        }
+                    }
+                }
+                
+            }
+            for (coltemp = column+3; coltemp <= columnRange+3; coltemp++) {
+                var aa = inputlist.getElementsByTagName("TR")[rowtemp];
+                var ab = aa.getElementsByTagName("TD")[coltemp];
+                var bb = ab.getElementsByTagName("input")[0];
+                for (rowtemp1 = row; rowtemp1 <= rowRange; rowtemp1++) {
+                    for (coltemp1 = column+3; coltemp1 <= columnRange+3; coltemp1++) {
+                        var ac = inputlist.getElementsByTagName("TR")[rowtemp1];
+                        var ad = ac.getElementsByTagName("TD")[coltemp1];
+                        var cc = ad.getElementsByTagName("input")[0];
+                        if((bb.value == cc.value)&&(bb.value != ""))
+                        {
+                            if(!((rowtemp == rowtemp1)&&(coltemp ==  coltemp1)))
+                            {
+                                bb.style.color = "red";
+                                cc.style.color = "red";
+                            }
+                        }
+                    }
+                }
+                
+            }
+            for (coltemp = column+6; coltemp <= columnRange+6; coltemp++) {
+                var aa = inputlist.getElementsByTagName("TR")[rowtemp];
+                var ab = aa.getElementsByTagName("TD")[coltemp];
+                var bb = ab.getElementsByTagName("input")[0];
+                for (rowtemp1 = row; rowtemp1 <= rowRange; rowtemp1++) {
+                    for (coltemp1 = column+6; coltemp1 <= columnRange+6; coltemp1++) {
+                        var ac = inputlist.getElementsByTagName("TR")[rowtemp1];
+                        var ad = ac.getElementsByTagName("TD")[coltemp1];
+                        var cc = ad.getElementsByTagName("input")[0];
+                        if((bb.value == cc.value)&&(bb.value != ""))
+                        {
+                            if(!((rowtemp == rowtemp1)&&(coltemp ==  coltemp1)))
+                            {
+                                bb.style.color = "red";
+                                cc.style.color = "red";
+                            }
+                        }
+                    }
+                }
+                
+            } 
+        }
+        
+    }
+
+
+
 
 
     function checkRangeOfInputs() {
@@ -61,17 +274,15 @@ window.onload = function () {
                 var b = a.getElementsByTagName("TD")[j];
                 var c = b.getElementsByTagName("input")[0];
                 if ((c.value > 0) && (c.value < 10)) {
-                    c.style.color = "yellow";
-                    c.style.backgroundColor = "black"
+                    c.style.color = "Green";
                     c.style.fontWeight = "bolder";
 
                 }
                 else if (c.value == "") {
-                    c.style.backgroundColor = "white"
+                    c.style.backgroundColor = "white";
                 }
                 else {
                     c.style.color = "red";
-                    c.style.backgroundColor = "black"
                     c.style.fontWeight = "bolder";
                 }
             }
